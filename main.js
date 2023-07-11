3ea2aea4b55cddde641d5c626ddbf35233597ce5
website <135435010+websitte123@users.noreply.github.com>
2023-07-11 09:17:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ function winner()
     if(squars[1] == squars[2] && squars[2] == squars[3] && squars[1] != '')
     {
         title.innerHTML=squars[1] + ' is the winner';
-        document.getElementById('item' + 1).style.background='rgb(255, 0, 68)';
-        document.getElementById('item' + 2).style.background='rgb(255, 0, 68)';
-        document.getElementById('item' + 3).style.background='rgb(255, 0, 68)';
+        document.getElementById('item' + 1).style.background='rgb(85, 255, 0)';
+        document.getElementById('item' + 2).style.background='rgb(85, 255, 0)';
+        document.getElementById('item' + 3).style.background='rgb(85, 255, 0)';
         setInterval(function(){title.innerHTML += '.'},1000);
         setTimeout(function(){location.reload()},3000);
         var audio0 = new Audio('winner.mp3');
@@ -20,9 +20,9 @@ function winner()
     else if(squars[4] == squars[5] && squars[5] == squars[6] && squars[4] != '')
     {
         title.innerHTML=squars[4] + ' is the winner';
-        document.getElementById('item' + 4).style.background='rgb(255, 0, 68)';
-        document.getElementById('item' + 5).style.background='rgb(255, 0, 68)';
-        document.getElementById('item' + 6).style.background='rgb(255, 0, 68)';
+        document.getElementById('item' + 4).style.background='rgb(85, 255, 0))';
+        document.getElementById('item' + 5).style.background='rgb(85, 255, 0)';
+        document.getElementById('item' + 6).style.background='rgb(85, 255, 0)';
         setInterval(function(){title.innerHTML += '.'},1000);
         setTimeout(function(){location.reload()},3000);
         var audio0 = new Audio('winner.mp3');
@@ -31,9 +31,9 @@ function winner()
     else if(squars[7] == squars[8] && squars[8] == squars[9] && squars[7] != '')
     {
         title.innerHTML=squars[7] + ' is the winner';
-        document.getElementById('item' + 7).style.background='rgb(255, 0, 68)';
-        document.getElementById('item' + 8).style.background='rgb(255, 0, 68)';
-        document.getElementById('item' + 9).style.background='rgb(255, 0, 68)';
+        document.getElementById('item' + 7).style.background='rgb(85, 255, 0)';
+        document.getElementById('item' + 8).style.background='rgb(85, 255, 0)';
+        document.getElementById('item' + 9).style.background='rgb(85, 255, 0)';
         setInterval(function(){title.innerHTML += '.'},1000);
         setTimeout(function(){location.reload()},3000);
         var audio0 = new Audio('winner.mp3');
@@ -42,9 +42,9 @@ function winner()
     else if(squars[1] == squars[4] && squars[4] == squars[7] && squars[1] != '')
     {
         title.innerHTML=squars[1] + ' is the winner';
-        document.getElementById('item' + 1).style.background='rgb(255, 0, 68)';
-        document.getElementById('item' + 4).style.background='rgb(255, 0, 68)';
-        document.getElementById('item' + 7).style.background='rgb(255, 0, 68)';
+        document.getElementById('item' + 1).style.background='rgb(85, 255, 0)';
+        document.getElementById('item' + 4).style.background='rgb(85, 255, 0)';
+        document.getElementById('item' + 7).style.background='rgb(85, 255, 0)';
         setInterval(function(){title.innerHTML += '.'},1000);
         setTimeout(function(){location.reload()},3000);
         var audio0 = new Audio('winner.mp3');
@@ -53,9 +53,9 @@ function winner()
     else if(squars[2] == squars[5] && squars[5] == squars[8] && squars[2] != '')
     {
         title.innerHTML=squars[2] + ' is the winner';
-        document.getElementById('item' + 2).style.background='rgb(255, 0, 68)';
-        document.getElementById('item' + 5).style.background='rgb(255, 0, 68)';
-        document.getElementById('item' + 8).style.background='rgb(255, 0, 68)';
+        document.getElementById('item' + 2).style.background='rgb(85, 255, 0)';
+        document.getElementById('item' + 5).style.background='rgb(85, 255, 0)';
+        document.getElementById('item' + 8).style.background='rgb(85, 255, 0)';
         setInterval(function(){title.innerHTML += '.'},1000);
         setTimeout(function(){location.reload()},3000);
         var audio0 = new Audio('winner.mp3');
@@ -64,9 +64,9 @@ function winner()
     else if(squars[3] == squars[6] && squars[6] == squars[9] && squars[3] != '')
     {
         title.innerHTML=squars[3] + ' is the winner';
-        document.getElementById('item' + 3).style.background='rgb(255, 0, 68)';
-        document.getElementById('item' + 6).style.background='rgb(255, 0, 68)';
-        document.getElementById('item' + 9).style.background='rgb(255, 0, 68)';
+        document.getElementById('item' + 3).style.background='rgb(85, 255, 0)';
+        document.getElementById('item' + 6).style.background='rgb(85, 255, 0)';
+        document.getElementById('item' + 9).style.background='rgb(85, 255, 0)';
         setInterval(function(){title.innerHTML += '.'},1000);
         setTimeout(function(){location.reload()},3000);
         var audio0 = new Audio('winner.mp3');
@@ -75,9 +75,9 @@ function winner()
     else if(squars[1] == squars[5] && squars[5] == squars[9] && squars[1] != '')
     {
         title.innerHTML=squars[1] + ' is the winner';
-        document.getElementById('item' + 1).style.background='rgb(255, 0, 68)';
-        document.getElementById('item' + 5).style.background='rgb(255, 0, 68)';
-        document.getElementById('item' + 9).style.background='rgb(255, 0, 68)';
+        document.getElementById('item' + 1).style.background='rgb(85, 255, 0)';
+        document.getElementById('item' + 5).style.background='rgb(85, 255, 0)';
+        document.getElementById('item' + 9).style.background='rgb(85, 255, 0)';
         setInterval(function(){title.innerHTML += '.'},1000);
         setTimeout(function(){location.reload()},3000);
         var audio0 = new Audio('winner.mp3');
@@ -86,13 +86,20 @@ function winner()
     else if(squars[3] == squars[5] && squars[5] == squars[7] && squars[3] != '')
     {
         title.innerHTML=squars[3] + ' is the winner';
-        document.getElementById('item' + 3).style.background='rgb(255, 0, 68)';
-        document.getElementById('item' + 5).style.background='rgb(255, 0, 68)';
-        document.getElementById('item' + 7).style.background='rgb(255, 0, 68)';
+        document.getElementById('item' + 3).style.background='rgb(85, 255, 0)';
+        document.getElementById('item' + 5).style.background='rgb(85, 255, 0)';
+        document.getElementById('item' + 7).style.background='rgb(85, 255, 0)';
         setInterval(function(){title.innerHTML += '.'},1000);
         setTimeout(function(){location.reload()},3000);
         var audio0 = new Audio('winner.mp3');
         audio0.play();
+    }
+    else if(squars[1] !='' && squars[2] !='' &&squars[3] !='' && squars[4] !='' && squars[5] !='' && squars[6] !='' && squars[7] !='' && squars[8] !='' &&squars[9] !='')
+    {
+        title.innerHTML='no one is winner';
+        setInterval(function(){title.innerHTML += '.'},1000);
+        setTimeout(function(){location.reload()},3000);
+
     }
 }
 
